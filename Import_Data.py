9086b71b08a5f3d2a971d12952ee65432e466612
SofianeMD <se.mehd28@gmail.com>
2026-02-19 11:37:55 +0100

    Create Import_Data.py

---
+++ Import_Data.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import pandas as pd
+from openpyxl import load_workbook
+import os
+
+FILE_NAME = "/Users/sofiane/Desktop/Analyse_Portefeuille.xlsm"
+SHEET_NAME = "Data_Finance"
+TK = ["AAPL", "NVDA"]
+
+def update_excel():
+    try:
+       
+        df = yf.download(TK, period="60d")['Close'].dropna()
+        
+        if not os.path.exists(FILE_NAME):
+            print("Erreur : Le fichier Excel n'a pas été trouvé.")
+            return
+
+        book = load_workbook(FILE_NAME, keep_vba=True)
+        
+        with pd.ExcelWriter(FILE_NAME, engine='openpyxl') as writer:
+            writer.book = book
+
+            if SHEET_NAME in book.sheetnames:
+                book.remove(book[SHEET_NAME])
+            df.to_excel(writer, sheet_name=SHEET_NAME)
+            
+        print("Mise à jour terminée.)")
+        
+    except Exception as e:
+        print(f"Erreur : {e}")
+
+if __name__ == "__main__":
+    update_excel()
